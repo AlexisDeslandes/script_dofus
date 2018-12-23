892c9185944c08ec5ada512bc3695ad8fbf06de2
AlexisDeslandes <alexis.deslandes@etu.unice.fr>
2018-12-23 10:29:46 +0000

    Initialisation script

---
+++ installation.sh
@@ -0,0 +1,13 @@
+sudo su
+apt-get install git
+apt-get update
+apt-get upgrade
+apt-get install git
+apt-get install playonlinux
+git clone https://github.com/AlexisDeslandes/script_dofus.git
+cd script_dofus
+apt-get install python-pip
+pip install pymouse
+apt-get install  python-xlib
+pip install pyscreenshot
+pip install Image
